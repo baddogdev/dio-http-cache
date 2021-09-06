489b0944e127d30d818e8929de381d358d28fd98
nfz <nfz@github.com>
2021-09-06 16:25:00 +0800

    check box is open

--- lib/src/store/store_hive.dart
+++ lib/src/store/store_hive.dart
@@ -9,14 +9,18 @@ class StoreHive extends ICacheStore {
   StoreHive();
 
   /// control hive box external
-  Box<CacheObj> _getBox() {
-    return Hive.box<CacheObj>(storeName);
+  Box<CacheObj>? _getBox() {
+    if (Hive.isBoxOpen(storeName)) {
+      return Hive.box<CacheObj>(storeName);
+    }
+
+    return null;
   }
 
   @override
   Future<bool> clearAll() async {
     var box = _getBox();
-    box.clear();
+    box?.clear();
 
     return true;
   }
@@ -24,6 +28,10 @@ class StoreHive extends ICacheStore {
   @override
   Future<bool> clearExpired() async {
     var box = _getBox();
+    if (box == null) {
+      return true;
+    }
+
     var now = DateTime.now().millisecondsSinceEpoch;
     for (var i = box.length - 1; i >= 0; i--) {
       var obj = box.getAt(i)!;
@@ -39,6 +47,10 @@ class StoreHive extends ICacheStore {
   @override
   Future<bool> delete(String key, {String? subKey}) async {
     var box = _getBox();
+    if (box == null) {
+      return true;
+    }
+
     String objKey = "${key}_$subKey";
     await box.delete(objKey);
 
@@ -48,6 +60,10 @@ class StoreHive extends ICacheStore {
   @override
   Future<CacheObj?> getCacheObj(String key, {String? subKey = ""}) async {
     var box = _getBox();
+    if (box == null) {
+      return null;
+    }
+
     String objKey = "${key}_$subKey";
     var cacheObj = box.get(objKey);
     return cacheObj;
@@ -56,6 +72,10 @@ class StoreHive extends ICacheStore {
   @override
   Future<bool> setCacheObj(CacheObj obj) async {
     var box = _getBox();
+    if (box == null) {
+      return true;
+    }
+
     String objKey = "${obj.key}_${obj.subKey}";
     await box.put(objKey, obj);
 
